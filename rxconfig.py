ef697f73bfd20c7033b6452312327d907920a58b
Espinoza Enrique Manuel <espinoza.enrique.87@gmail.com>
2025-12-13 20:24:33 -0300

    modifico para probar instalar en Render. Daba error de falta de memoria

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="tiny_reflex",
-    plugins=[rx.plugins.SitemapPlugin()],
+    #plugins=[rx.plugins.SitemapPlugin()],
     backend_host="0.0.0.0",
     #backend_port=8000
 )
